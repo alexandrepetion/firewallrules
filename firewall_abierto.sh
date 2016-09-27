5ef79531670db1d807da6f4b68c0a5e27ecf0dd6
Alexandre <jkeos8@aol.com>
2016-09-26 19:21:27 -0500

    Update firewall_abierto.sh

--- firewall_abierto.sh
+++ firewall_abierto.sh
@@ -1,12 +1,14 @@
 #Taller de Iptables
-#Julio Cesar Gomez Castano
-#agosto 14  del 2016
+#Profesor: Julio Cesar Gomez Castano
+#Alumno: Julian Andres Valencia M.
+#Curso: Seguridad en Datos.
+#Esp. Seguridad  Informática. 
+#Universidad Autonoma de Occidente.
+#Fecha: 24 de Septimbre 2016
 
 #Deja el firewall con todo abierto
-
 #Borrar las reglas
 iptables -F
-
 
 #Fijar las politicas en permitir
 iptables -P INPUT ACCEPT
